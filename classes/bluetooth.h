58457f01db5ec394a693b0c6de8bbf317daffe8c
鐘妘甄 <shirley@mail.kway.com.tw>
2017-07-10 13:39:37 +0800

    init commit

---
+++ classes/bluetooth.h
@@ -0,0 +1,10 @@
+#ifndef _classes_for_using_bluetooth_func_on_windows_201707_sly
+#define _classes_for_using_bluetooth_func_on_windows_201707_sly
+
+#include <Bthsdpdef.h>
+#include <BluetoothAPIs.h>
+#include <Winsock2.h>
+#include <Ws2bth.h>
+
+
+#endif
